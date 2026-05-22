a809560a0d717f12b698618437daace76773aee9
Xerovo228 <aevloev839@gmail.com>
2026-05-22 15:53:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
 
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbztD215U09edQ837xmYPzcCWxQTz7e7K2FIgs97e7vNbNDiTowqbzYrs9soVOWB5ApIlw/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwf5orbprxSlESRAlj2ESuWqslmVYKcyRmKzI9aOe6SdfvKL4-IZRneKcN9QqLkzVW4RQ/exec";
 
 let currentRole = 'student';
 let currentFilter = 'all';
